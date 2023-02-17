40fcdc6ca2a5761f31eb5c501a488d4bc55bca88
stephanus chung <chungsy3@gmail.com>
2023-02-16 20:02:38 -0500

    started with other CRUD for favorites

--- sample_service/routers/accounts.py
+++ sample_service/routers/accounts.py
@@ -46,8 +46,36 @@ async def create_accounts(
     return AccountToken(account=account, **token.dict())
 
 
+@router.get("/accounts")
+def get_accounts(
+    # account: Account,
+    repo: AccountRepository = Depends()
+    ):
+    return {
+        "accounts": repo.get_all(),
+    }
+    
+
+@router.get("/accounts/{username}")
+def get_account(
+    username: str,
+    # account: Account,
+    repo: AccountRepository = Depends()
+    ):
+    return repo.get(username)
 
 
+@router.get("/token", response_model=AccountToken | None)
+async def get_token(
+    request: Request,
+    account: AccountOut = Depends(authenticator.try_get_current_account_data)
+) -> AccountToken | None:
+    if account and authenticator.cookie_name in request.cookies:
+        return {
+            "access_token": request.cookies[authenticator.cookie_name],
+            "type": "Bearer",
+            "account": account,
+        }
 
 # @router.post("/api/accounts", response_model=AccountToken | HttpError)
 # async def create_account(
@@ -71,42 +99,3 @@ async def create_accounts(
 
 
 
-
-@router.get("/accounts")
-def get_accounts(
-    # account: Account,
-    repo: AccountRepository = Depends()
-    ):
-    return {
-        "accounts": repo.get_all(),
-    }
-    
-
-@router.get("/accounts/{username}")
-def get_account(
-    username: str,
-    # account: Account,
-    repo: AccountRepository = Depends()
-    ):
-    return repo.get(username)
-
-
-
-    detail: str
-
-
-
-
-
-# @router.get("/token", response_model=AccountToken | None)
-# async def get_token(
-#     request: Request,
-#     account: Account = Depends(authenticator.try_get_current_account_data)
-# ) -> AccountToken | None:
-#     if account and authenticator.cookie_name in request.cookies:
-#         return {
-#             "access_token": request.cookies[authenticator.cookie_name],
-#             "type": "Bearer",
-#             "account": account,
-#         }
-  
