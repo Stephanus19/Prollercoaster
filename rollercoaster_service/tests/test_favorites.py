6e5fc684962ef3b8b3ac2fa0c8e6f4179733f94b
Alexis <alexisweaver28@gmail.com>
2023-03-07 17:15:59 -0500

    completed test get favorites by user

--- rollercoaster_service/tests/test_favorites.py
+++ rollercoaster_service/tests/test_favorites.py
@@ -21,10 +21,10 @@ class FakeFavoritesRepository:
         return Favorites(id=1, user_id=12, **favorites_dict)
 
     def get_favorites_by_user(self, user_id: int):
-        pass
+        return [{"id": 1, "user_id": 12, "rollercoaster_id": 7}]
 
     def delete_favorite(self, rollercoaster_id: int):
-        pass
+        return True
 
 
 def test_create():
@@ -40,3 +40,17 @@ def test_create():
     assert res.json()["rollercoaster_id"] == 7
 
     app.dependency_overrides = {}
+
+def test_get_favorites_by_user():
+    app.dependency_overrides[FavoritesRepository] = FakeFavoritesRepository
+    app.dependency_overrides[authenticator.get_current_account_data] = fake_get_current_account_data
+
+    res = client.get('/favorites')
+
+    assert res.status_code == 200
+    assert res.json() == [{"id": 1, "user_id": 12, "rollercoaster_id": 7}]
+
+    app.dependency_overrides = {}
+
+def test_delete_favorite():
+    pass
